aca7ebd4c8a09c8deb204283e0ab62fcdcb8fe83
Maimer <nesnl23@yahoo.com>
2014-06-02 17:56:14 -0400

    Game complete, all methods work.  Refactoring complete.

--- minefield.rb
+++ minefield.rb
@@ -6,55 +6,93 @@ class Minefield
     @row_count = row_count
     @mine_count = mine_count
     @field = create_field
+    @board = create_board
   end
 
   def create_field
     blanks = [0] * ((@row_count * @column_count) - @mine_count)
     mines = [1] * @mine_count
-    field = blanks.concat(mines).shuffle.each_slice(@row_count).to_a
+    blanks.concat(mines).shuffle.each_slice(@row_count).to_a
   end
 
-  # Uncover the given cell. If there are no adjacent mines to this cell
-  # it should also clear any adjacent cells as well. This is the action
-  # when the player clicks on the cell.
+  def create_board
+    ([0] * (@row_count * @column_count)).each_slice(@row_count).to_a
+  end
+
   def clear(row, col)
-    cell = @field[row][col]
+    @board[row][col] = 1
+    if adjacent_mines(row, col) != 0 || @field[row][col] == 1
+      return false
+    else
+      surrounding_cells(row, col)
+    end
   end
 
-  # Check if any cells have been uncovered that also contained a mine. This is
-  # the condition used to see if the player has lost the game.
+  def clear_adjacent(row, col)
+    if @board[row][col] == 0 && @field[row][col] == 0 && adjacent_mines(row, col) == 0
+      @board[row][col] = 1
+      surrounding_cells(row, col)
+    else
+      @board[row][col] = 1
+    end
+  end
+
+  def surrounding_cells(row, col)
+    (-1..1).each do |i|
+      (-1..1).each do |j|
+        if !(i == 0 && j == 0)
+          if board_check?(row+i, col+j) then clear_adjacent(row+i, col+j) end
+        end
+      end
+    end
+  end
+
   def any_mines_detonated?
-
+    @field.each_with_index do |row, i|
+      row.each_with_index do |col, j|
+        if @field[i][j] == 1 && @board[i][j] == 1
+          return true
+        end
+      end
+    end
     false
   end
 
-  # Check if all cells that don't have mines have been uncovered. This is the
-  # condition used to see if the player has won the game.
   def all_cells_cleared?
-
-    false
+    blanks = 0
+    @field.each_with_index do |row, i|
+      row.each_with_index do |col, j|
+        if @board[i][j] == 1
+          blanks += 1
+        end
+      end
+    end
+    blanks == ((@row_count * @column_count) - @mine_count)
   end
 
-  # Return true if the cell been uncovered, false otherwise.
   def cell_cleared?(row, col)
-
-    false
+    @board[row][col] == 1
   end
 
   def contains_mine?(row, col)
     @field[row][col] == 1
   end
 
+  def board_check?(row, col)
+    row >= 0 && col >= 0 && row < @row_count && col < @column_count
+  end
+
   def adjacent_mines(row, col)
     mines = 0
-
-    if !@field[row][col-1].nil? then mines += @field[row][col-1]
-    if !@field[row][col+1].nil? then mines += @field[row][col+1]
-    if !@field[row-1][col-1].nil? then mines += @field[row-1][col-1]
-    if !@field[row-1][col].nil? then mines += @field[row-1][col]
-    if !@field[row-1][col+1].nil? then mines += @field[row-1][col+1]
-    if !@field[row+1][col-1].nil? then mines += @field[row+1][col-1]
-    if !@field[row+1][col].nil? then mines += @field[row+1][col]
-    if !@field[row+1][col+1].nil? then mines += @field[row+1][col+1]
+    (-1..1).each do |i|
+      (-1..1).each do |j|
+        if !(i == 0 && j == 0)
+          if board_check?(row+i, col+j) then mines += @field[row+i][col+j] end
+        end
+      end
+    end
+    mines
   end
 end
+
+
